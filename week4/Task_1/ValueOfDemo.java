9cb4cb051c13899e8201d2187a2ff87b77434d85
Tom Lawton <vnminhduy0750@outlook.com>
2022-11-20 23:41:37 +0700

    Update final tasks of week 4

---
+++ week4/Task_1/ValueOfDemo.java
@@ -0,0 +1,11 @@
+public class ValueOfDemo {
+    public static void main(String[] args) {
+        float a = (Float.valueOf(args[0])).floatValue();
+        float b = (Float.valueOf(args[1])).floatValue();
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+    }
+}
